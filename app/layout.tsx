f009e0d8c1bb381d0aced8d9ac8d15a39f315860
Teslyuk Yurii <yurickv@gmail.com>
2025-05-16 13:46:52 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Investor Dashboard",
+  description: "Simplified investor dashboard using Supabase and Next.js",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='bg-gray-50 text-gray-900 font-sans'>{children}</body>
+    </html>
+  );
+}
